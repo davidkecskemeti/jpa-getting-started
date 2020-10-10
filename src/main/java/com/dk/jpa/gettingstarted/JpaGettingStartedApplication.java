c033b747362d39725dda51420e2693b1f15231f4
Dávid Kecskeméti <dxdy421@gmail.com>
2020-10-10 13:08:34 +0200

    Initial commit

---
+++ src/main/java/com/dk/jpa/gettingstarted/JpaGettingStartedApplication.java
@@ -0,0 +1,13 @@
+package com.dk.jpa.gettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaGettingStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaGettingStartedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dk/jpa/gettingstarted/JpaGettingStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.jpa.gettingstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaGettingStartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
